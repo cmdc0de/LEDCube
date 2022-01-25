3609bd4ea1944829385c50c62f4dd33dfafcae06
cmdc0de <cmdc0dez@gmail.com>
2022-01-25 00:39:28 -0700

    sta_connect works

--- apa106/prototype1/main/app.cpp
+++ apa106/prototype1/main/app.cpp
@@ -16,6 +16,9 @@
 #include "freertos.h"
 #include "fatfsvfs.h"
 #include "pinconfig.h"
+#include "etl/vector.h"
+#include "net/wifi.h"
+#include "net/wifieventhandler.h"
 
 using libesp::ErrorType;
 using libesp::System;
@@ -47,12 +50,55 @@ MyApp::MyApp() : AppErrors(), CurrentMode(ONE), LastTime(0) {
 MyApp::~MyApp() {
 
 }
-  
-NoClkLedStrip LedControl = NoClkLedStrip::create(APA106::get(),127,9);
 
+NoClkLedStrip LedControl = NoClkLedStrip::create(APA106::get(),127,9);
+libesp::WiFiEventHandler MyHandler;
+
+#define TEST_CODE
 libesp::ErrorType MyApp::onInit() {
 	ErrorType et;
 	ESP_LOGI(LOGTAG,"OnInit: Free: %u, Min %u", System::get().getFreeHeapSize(),System::get().getMinimumFreeHeapSize());
+#ifdef TEST_CODE
+  //esp_netif_init();
+  //esp_event_loop_create_default();
+  //esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+  //assert(sta_netif);
+
+  //wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  //esp_wifi_init(&cfg);
+  //ESP_LOGI(LOGTAG,"after wifi config");
+
+  libesp::WiFi wifi;
+  wifi.setWifiEventHandler(&MyHandler);
+	ESP_LOGI(LOGTAG,"OnInit: Free: %u, Min %u", System::get().getFreeHeapSize(),System::get().getMinimumFreeHeapSize());
+  et = wifi.init(WIFI_MODE_STA);
+	ESP_LOGI(LOGTAG,"OnInit: Free: %u, Min %u", System::get().getFreeHeapSize(),System::get().getMinimumFreeHeapSize());
+  if(et.ok()) {
+    etl::vector<libesp::WiFiAPRecord,16> results;
+    wifi_scan_config_t conf;
+    memset(&conf,0,sizeof(conf));
+    conf.show_hidden = true;
+    conf.scan_type = WIFI_SCAN_TYPE_PASSIVE; //WIFI_SCAN_TYPE_ACTIVE
+	  ESP_LOGI(LOGTAG,"OnInit: Free: %u, Min %u", System::get().getFreeHeapSize(),System::get().getMinimumFreeHeapSize());
+    et = wifi.scan(results, conf);
+	  ESP_LOGI(LOGTAG,"OnInit: Free: %u, Min %u", System::get().getFreeHeapSize(),System::get().getMinimumFreeHeapSize());
+    etl::vector<libesp::WiFiAPRecord,16>::iterator it;
+    for(it=results.begin();it!=results.end();++it) {
+      ESP_LOGI(LOGTAG,"%s", (*it).toString().c_str());
+    }
+  }
+  const char * ssid = "dac-nh";
+  const char * pass = "DemetriusAndAnnemarie%0320";
+  et = wifi.connect(ssid,pass,WIFI_AUTH_OPEN);
+  if(!et.ok()) {
+    ESP_LOGI(LOGTAG, "connect error: %d %s", et.getErrT(), et.toString());
+  } else {
+    ESP_LOGI(LOGTAG, "connect call successful");
+  }
+  
+  return et;
+
+#else
 
 	et = LedControl.init(GPIO_NUM_19,static_cast<rmt_channel_t>(0));
 	if(!et.ok()) {
@@ -70,12 +116,13 @@ libesp::ErrorType MyApp::onInit() {
     LastTime = FreeRTOS::getTimeSinceStart();
 
 	return et;
+#endif
 }
 
 static uint32_t count = 1;
 ErrorType MyApp::onRun() {
-#if 0
-		  return ErrorType();
+#ifdef TEST_CODE
+  return ErrorType();
 #else
   ErrorType et;
   uint32_t timeSinceLast = FreeRTOS::getTimeSinceStart()-LastTime;
